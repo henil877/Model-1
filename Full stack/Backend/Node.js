4c682df137cef78b1a9a7cb9eaa2cff19c665adb
henil877 <henildarji94@gmail.com>
2026-05-27 17:28:14 +0530

    27-may

--- Full stack/Backend/Node.js
+++ Full stack/Backend/Node.js
@@ -1,42 +1,77 @@
-var http = require('http');
-var fs = require('fs');
+// var http = require('http');
+// var fs = require('fs');
 
-http.createServer((req, res) => {
+// http.createServer((req, res) => {
 
-    res.writeHead(200, { 'Content-Type': 'text/html' });
+//     res.writeHead(200, { 'Content-Type': 'text/html' });
 
-    if(req.url == "/")
-    {
-        res.end("<h1>Welcome in Home page...</h1>");
-    }
+//     if(req.url == "/")
+//     {
+//         res.end("<h1>Welcome in Home page...</h1>");
+//     }
 
-    else if(req.url == "/pr2")
-    {
-        fs.readFile('./pr2.html', (err, data) => {
+//     else if(req.url == "/pr2")
+//     {
+//         fs.readFile('./pr2.html', (err, data) => {
 
-            if(err)
-                throw err;
+//             if(err)
+//                 throw err;
 
-            res.end(data);
-        });
-    }
+//             res.end(data);
+//         });
+//     }
 
-    else if(req.url == "/pr9")
-    {
-        fs.readFile('./pr9.html', (err, data) => {
+//     else if(req.url == "/pr9")
+//     {
+//         fs.readFile('./pr9.html', (err, data) => {
 
-            if(err)
-                throw err;
+//             if(err)
+//                 throw err;
 
-            res.end(data);
-        });
-    }
+//             res.end(data);
+//         });
+//     }
 
-    else
-    {
-        res.end("Page not found");
-    }
+//     else
+//     {
+//         res.end("Page not found");
+//     }
 
-}).listen(8081);
+// }).listen(8081);
 
-console.log("Server running on port 8081");
+// console.log("Server running on port 8081");
+
+
+// Express.js
+
+// const express = require('express');
+
+// const app = express();
+
+// app.get('/', (req, res) => {
+//     res.send("Welcome in home");
+// });
+
+// app.get('/pr2', (req, res) => {
+//     res.sendFile(__dirname + '/pr2.html');
+// });
+
+// app.listen(5050, () => {
+//     console.log("Server running on port 5050");
+// });
+
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'Public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'Public', 'index.html'));
+});
+
+app.listen(8081, () => {
+    console.log('Server running on http://localhost:8081');
+});
