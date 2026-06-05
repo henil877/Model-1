a6037eb5c5a8252a96395f366e394a7d7c2b2a84
henil877 <henildarji94@gmail.com>
2026-06-05 11:06:22 +0530

    All Work

--- Full stack/Frontend/src/App.js
+++ Full stack/Frontend/src/App.js
@@ -1,15 +1,23 @@
-import React from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
 
 import Header from "./Header";
 import ApiCalling from "./ApiCalling";
 import Showuser from "./Pages/Showuser";
 import Home from "./Pages/Home";
 
+function RedirectToRegister() {
+  useEffect(() => {
+    window.location.href = "/model_1_StaticWeb/Register.html";
+  }, []);
+
+  return null;
+}
+
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/model_1_StaticWeb/Register.html" replace />} />
+      <Route path="/" element={<RedirectToRegister />} />
       <Route path="/home" element={<Home />} />
       <Route path="/Header" element={<Header />} />
       <Route path="/ApiCalling" element={<ApiCalling />} />
